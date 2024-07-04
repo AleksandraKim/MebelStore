f0767b0b05b16491bede2ae16186ee4bbaa8ce82
AleksandraKim <119132509+AleksandraKim@users.noreply.github.com>
2024-07-04 16:13:44 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -46,7 +46,7 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
  
-    <RouterProvider basename="/MebelStore" router={router}/>
+    <RouterProvider router={router}/>
    
   </React.StrictMode>
 );
